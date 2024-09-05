c9a7f1c2d1a8893f0d9298d65f00779d1febf416
[cocosky77] <[skybella@naver.com]>
2024-09-05 16:26:48 +0900

    message

--- src/App.js
+++ src/App.js
@@ -1,119 +1,158 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
-  const poster = [
-    {name: "poster1", imageUrl: "https://via.placeholder.com/150x225.png?text=Poster+1"},
-    {name: "poster2", imageUrl: "https://via.placeholder.com/150x225.png?text=Poster+2"},
-    {name: "poster3", imageUrl: "https://via.placeholder.com/150x225.png?text=Poster+3"},
-    {name: "poster4", imageUrl: "https://via.placeholder.com/150x225.png?text=Poster+4"},
-    {name: "poster5", imageUrl: "https://via.placeholder.com/150x225.png?text=Poster+5"},
-    {name: "poster6", imageUrl: "https://via.placeholder.com/150x225.png?text=Poster+6"},
-    {name: "poster7", imageUrl: "https://via.placeholder.com/150x225.png?text=Poster+7"},
-    {name: "poster8", imageUrl: "https://via.placeholder.com/150x225.png?text=Poster+8"},
-    {name: "poster9", imageUrl: "https://via.placeholder.com/150x225.png?text=Poster+9"},
+  // const [isAnimating, setIsAnimating] = useState(false);
+  // const handleMouseEnter = () => {
+  //   setIsAnimating(true);
+  // };
+  // const handleMouseLeave = () => {
+  //   setIsAnimating(false);
+  // };
 
+  const poster = [
+    {name: "poster1", url: "https://via.placeholder.com/250x400.png?text=Poster+1"},
+    {name: "poster2", url: "https://via.placeholder.com/250x400.png?text=Poster+2"},
+    {name: "poster3", url: "https://via.placeholder.com/250x400.png?text=Poster+3"},
+    {name: "poster4", url: "https://via.placeholder.com/250x400.png?text=Poster+4"},
+    {name: "poster5", url: "https://via.placeholder.com/250x400.png?text=Poster+5"},
+    {name: "poster6", url: "https://via.placeholder.com/250x400.png?text=Poster+6"},
   ];
 
-  const array = [{
+  const tournaments = [{
     name : "Miracle Game",
-    imageUrl: "https://via.placeholder.com/225x150.png?text=Poster+1",
-    value : 1000,
-    dolor : 1000.51,
+    url: "https://via.placeholder.com/300x200.png?text=Poster+1",
+    date : new Date().toLocaleDateString(),
+    dolor : 1000,
   },{
     name : "Miracle Game2",
-    imageUrl: "https://via.placeholder.com/225x150.png?text=Poster+2",
-    value : 2000,
-    dolor : 2000.51,
+    url: "https://via.placeholder.com/300x200.png?text=Poster+2",
+    date : new Date().toLocaleDateString(),
+    dolor : 1000,
   },{
     name : "Miracle Game3",
-    imageUrl: "https://via.placeholder.com/225x150.png?text=Poster+3",
-    value : 3000,
-    dolor : 3000.51,
+    url: "https://via.placeholder.com/300x200.png?text=Poster+3",
+    date : new Date().toLocaleDateString(),
+    dolor : 1000,
   },{
     name : "Miracle Game4",
-    imageUrl: "https://via.placeholder.com/225x150.png?text=Poster+4",
-    value : 4000,
-    dolor : 4000.51,
+    url: "https://via.placeholder.com/300x200.png?text=Poster+4",
+    date : new Date().toLocaleDateString(),
+    dolor : 1000,
   },{
     name : "Miracle Game5",
-    imageUrl: "https://via.placeholder.com/225x150.png?text=Poster+5",
-    value : 5000,
-    dolor : 5000.51,
+    url: "https://via.placeholder.com/300x200.png?text=Poster+5",
+    date : new Date().toLocaleDateString(),
+    dolor : 1000,
   },{
     name : "Miracle Game6",
-    imageUrl: "https://via.placeholder.com/225x150.png?text=Poster+6",
-    value : 6000,
-    dolor : 6000.51,
+    url: "https://via.placeholder.com/300x200.png?text=Poster+6",
+    date : new Date().toLocaleDateString(),
+    dolor : 1000,
   },{
     name : "Miracle Game7",
-    imageUrl: "https://via.placeholder.com/225x150.png?text=Poster+7",
-    value : 7000,
-    dolor : 7000.51,
+    url: "https://via.placeholder.com/300x200.png?text=Poster+7",
+    date : new Date().toLocaleDateString(),
+    dolor : 1000,
   },{
     name : "Miracle Game8",
-    imageUrl: "https://via.placeholder.com/225x150.png?text=Poster+8",
-    value : 8000,
-    dolor : 8000.51,
+    url: "https://via.placeholder.com/300x200.png?text=Poster+8",
+    date : new Date().toLocaleDateString(),
+    dolor : 1000,
   },{
     name : "Miracle Game9",
-    imageUrl: "https://via.placeholder.com/225x150.png?text=Poster+9",
-    value : 9000,
-    dolor : 9000.51,
+    url: "https://via.placeholder.com/300x200.png?text=Poster+9",
+    date : new Date().toLocaleDateString(),
+    dolor : 1000,
   },{
     name : "Miracle Game10",
-    imageUrl: "https://via.placeholder.com/225x150.png?text=Poster+10",
-    value : 10000,
-    dolor : 10000.51,
+    url: "https://via.placeholder.com/300x200.png?text=Poster+10",
+    date : new Date().toLocaleDateString(),
+    dolor : 1000,
   },{
     name : "Miracle Game11",
-    imageUrl: "https://via.placeholder.com/225x150.png?text=Poster+11",
-    value : 11000,
-    dolor : 11000.51,
+    url: "https://via.placeholder.com/300x200.png?text=Poster+11",
+    date : new Date().toLocaleDateString(),
+    dolor : 1000,
   },{
     name : "Miracle Game12",
-    imageUrl: "https://via.placeholder.com/225x150.png?text=Poster+12",
-    value : 12000,
-    dolor : 12000.51,
+    url: "https://via.placeholder.com/300x200.png?text=Poster+12",
+    date : new Date().toLocaleDateString(),
+    dolor : 1000,
   },];
   
   return (
-
   <div className='App'>
-    <div className='header'>
-      <h1>AVAILABLE GAMES</h1>
-      <p>Today Schedule</p>
-      <button className='header_btn'>See All</button>
-    </div>
+    {/* nav바 */}
+    <nav>
+      <div className='nav_left'>
+        <img className='nav_left_img' src='https://github.com/user-attachments/assets/0d256f86-bb9b-4d3d-98ea-56d47bc586fa'/>
+      </div>
 
-    <div className='games'>
-      {poster.map((item, index) => (
-        <div key={index} className='image'>
-            <img src={item.imageUrl} alt={item.name} />
+      <a href='#'>MAIN</a>
+      <a href='#'>GAME</a>
+      <a href='#'>SHOP</a>
+      <a href='#'>EXPLORER</a>
+      <a href='#'>CLUB</a>
+      <a href='#'>EARN</a>
+
+      <div className='nav_right'>
+      <a href='#'>LOG</a>
+      <a href='#'>ENG</a>
+      </div>
+    </nav>
+
+    <div className='container'>
+      {}
+      <div className='available-games'>
+        <div className='header'>
+          <h1>AVAILABLE GAMES</h1>
+          <p>Today Schedule</p>
+          <button className='available-games_btn'>See All</button>
         </div>
-      ))}
-    </div>
 
-    <div className='header'>
-      <h1>UPCOMING TOURNAMENTS</h1>
-    </div>
+        <div className='available-games_posters'>
+          {poster.map((item, index) => (
+            <div
+            key={index}
+            // className={`available-games_posters_detail ${isAnimating ? 'animate' : ''}`}
+            // onMouseEnter={handleMouseEnter}
+            // onMouseLeave={handleMouseLeave}
+            >
+              <img src={item.url} alt={item.name} />
+            </div>
+          ))};
+        </div>
+      </div>
 
-    <div className='tournaments-grid'>
-      {array.map((item, index) => (
-          <div className='tournament-card' key={index}>
-              <div className='tournament-image'>
-                <img src={item.imageUrl} alt={item.name}/>
+      {/* Upcoming Tournaments */}
+      <div className='upcoming-tournaments'>
+        <div className='header'>
+          <h1>UPCOMING TOURNAMENTS</h1>
+        </div>
+
+        <div className='upcoming-tournaments_games'>
+          {tournaments.map((item, index) => (
+            <div key={index} className='upcoming-tournaments_games_card'>
+
+              {/* Upcoming Tournaments - card 상단 */}
+              <div className='upcoming-tournaments_games_image'>
+                <img src={item.url} alt={item.name} />
               </div>
 
-              <div className='tournament-details'>
-                <h2>{item.name}</h2>
-                <p>{item.value} points</p>
+              {/* Upcoming Tournaments - card 하단 */}
+              <div className='upcoming-tournaments_games_detail'>
+                <h1>{item.name}</h1>
+                <p>{item.date}</p>
                 <p>${item.dolor}</p>
               </div>
-          </div>
-      ))}
-    </div>
+            </div>
+          ))}
+        </div>    
+      </div>
 
+    </div>
   </div>
   );
 }
